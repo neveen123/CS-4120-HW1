570675da1177814236918d3deae8ce9972ce7f8b
Jattucks <84241955+Jattucks@users.noreply.github.com>
2021-06-02 23:30:35 -0400

    Add files via upload

---
+++ CS 4120 HW1.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import random
+import math
+import operator
+import numpy as np
+from sklearn.neighbors import KNeighborsClassifier
+
+def loadDataset(filename, split):
+    trainingSet = []
+    testSet = []
+    df = pd.read_csv(filename, header=None)
+    array = df.to_numpy()
+    random.shuffle(array)
+    training_len = int(len(array)*split)
+    trainingSet = array[:training_len]
+    testSet = array[training_len:]
+    return trainingSet, testSet
+
+
+
+def euclideanDistance(instance1, instance2, length):
+	distance = 0
+	for x in range(length):
+		distance += pow((instance1[x] - instance2[x]), 2)
+	return math.sqrt(distance)
+
+
+def main():
+    trainingSet=[]
+    testSet=[]
+    predications = []
+    split = 0.67
+    url = 'https://raw.githubusercontent.com/ruiwu1990/CSCI_4120/master/KNN/iris.data'
+    trainingSet, testSet = loadDataset(url, 0.66)
+    print('Train set: ' + repr(len(trainingSet)))
+    print('Test set: ' + repr(len(testSet)))
+    
+    neigh = KNeighborsClassifier(n_neighbors=1)
+    neigh.fit(trainingSet, testSet)z
+    
+    #use predict method
+    #print(neigh.predict())
+    
+    #using kneighbors mehthod to find neighbors
+    #print(neigh.kneighbors(testSet[1], return_distance = False))
+    
+    #score method used for accuracy
+    #score(testSet, trainingSet, sample_weight = None)
+
+main()
