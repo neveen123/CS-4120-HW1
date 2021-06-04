62ecb26114df490a986ca9ac01c232a9f4f7c8d8
Jattucks <84241955+Jattucks@users.noreply.github.com>
2021-06-04 07:25:09 -0400

    Update CS 4120 HW1.py
    Update with working prediction. Checked manually for accuracy.

--- CS 4120 HW1.py
+++ CS 4120 HW1.py
@@ -4,42 +4,61 @@ import math
 import operator
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
+from sklearn import preprocessing
+from sklearn.model_selection import train_test_split
+
+from sklearn import metrics
 
 def loadDataset(filename, split):
     trainingSet = []
     testSet = []
+    #features_train = []
+    #features_test = []
+    #lables_train = []
+    #labels_test = []
     df = pd.read_csv(filename, header=None)
     array = df.to_numpy()
-    random.shuffle(array)
-    training_len = int(len(array)*split)
-    trainingSet = array[:training_len]
-    testSet = array[training_len:]
-    return trainingSet, testSet
-
-
-
-def euclideanDistance(instance1, instance2, length):
-	distance = 0
-	for x in range(length):
-		distance += pow((instance1[x] - instance2[x]), 2)
-	return math.sqrt(distance)
+    # : will get all rows, :-1 will get all columns except last column
+    trainingSet = array[:, :-1]
+    # : will get all rows, 4 will get only the last column
+    testSet = array[:, 4]
+    # train_test_split method will split the trainingSet 
+    # and testSet into features and lables. Both trainingSet
+    # and testSet will auto shuffled. test_size is the split 
+    # percentage that will go to the test sets.
+    features_train, features_test, lables_train, labels_test = train_test_split(trainingSet, testSet, test_size = split)
+    
+    return features_train, features_test, lables_train, labels_test
 
 
 def main():
+    #should be 2d array with 150 rows and 4 columns representing features
     trainingSet=[]
+    #should be 1d with 150 labels represented in a number format (Ex: 0 = setsona)
     testSet=[]
-    predications = []
-    split = 0.67
     url = 'https://raw.githubusercontent.com/ruiwu1990/CSCI_4120/master/KNN/iris.data'
-    trainingSet, testSet = loadDataset(url, 0.66)
+    # features_train and features_lables represents the training dataset 
+    # of features and their correct lables that will be used by the machine
+    # for making predictions and checking accuracy.
+    #
+    # features_test and lables_test represent the datasets that will be used
+    # to test the machine
+    features_train, features_test, lables_train, labels_test = loadDataset(url, 0.66)
+    
+    
     
     neigh = KNeighborsClassifier(n_neighbors=1)
+    neigh.fit(features_train, lables_train)
+    
+    print(features_test)
     
     #use predict method
-    #print(neigh.predict())
+    prediction = neigh.predict(features_test)
+    
+    print(prediction)
     
     #using kneighbors mehthod to find neighbors
-    #print(neigh.kneighbors(testSet[1], return_distance = False))
+    #print(neigh.kneighbors(testSet, return_distance = False))
     
     #score method used for accuracy
     #score(testSet, trainingSet, sample_weight = None)
